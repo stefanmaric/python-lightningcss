d98a060d83b52954e05744283cd3083cd8c6e64b
Stefan Maric <me@stefanmaric.com>
2024-06-27 18:00:38 +0200

    Format with cargo

--- src/lib.rs
+++ src/lib.rs
@@ -39,29 +39,51 @@ use lightningcss::targets::{Browsers, Targets};
     browsers_list=None,
     minify=true,
 ))]
-fn process_stylesheet(code: &str,
-                      filename: &str,
-                      error_recovery: bool,
-                      parser_flags: u8,
-                      unused_symbols: Option<HashSet<String>>,
-                      browsers_list: Option<Vec<String>>,
-                      minify: bool) -> PyResult<String> {
-
+fn process_stylesheet(
+    code: &str,
+    filename: &str,
+    error_recovery: bool,
+    parser_flags: u8,
+    unused_symbols: Option<HashSet<String>>,
+    browsers_list: Option<Vec<String>>,
+    minify: bool,
+) -> PyResult<String> {
     let targets = match mk_targets(browsers_list) {
         Ok(val) => val,
-        Err(e) => {return Err(PyValueError::new_err(format!("browsers_list failed validation: {}", e.to_string())))}
+        Err(e) => {
+            return Err(PyValueError::new_err(format!(
+                "browsers_list failed validation: {}",
+                e.to_string()
+            )))
+        }
     };
-    let mut stylesheet = match StyleSheet::parse(code, mk_parser_options(filename, error_recovery, parser_flags)) {
+    let mut stylesheet = match StyleSheet::parse(
+        code,
+        mk_parser_options(filename, error_recovery, parser_flags),
+    ) {
         Ok(val) => val,
-        Err(e) => {return Err(PyValueError::new_err(format!("Parsing stylesheet failed: {}", e.to_string())))}
+        Err(e) => {
+            return Err(PyValueError::new_err(format!(
+                "Parsing stylesheet failed: {}",
+                e.to_string()
+            )))
+        }
     };
     match stylesheet.minify(mk_minify_options(unused_symbols, &targets)) {
         Ok(_) => {}
-        Err(e) => {return Err(PyValueError::new_err(format!("Minifying stylesheet failed: {}", e.to_string())));}
+        Err(e) => {
+            return Err(PyValueError::new_err(format!(
+                "Minifying stylesheet failed: {}",
+                e.to_string()
+            )));
+        }
     }
     return match stylesheet.to_css(mk_printer_options(&targets, minify)) {
         Ok(val) => Ok(val.code),
-        Err(e) => Err(PyValueError::new_err(format!("Printing stylesheet failed: {}", e.to_string())))
+        Err(e) => Err(PyValueError::new_err(format!(
+            "Printing stylesheet failed: {}",
+            e.to_string()
+        ))),
     };
 }
 
@@ -70,15 +92,19 @@ fn process_stylesheet(code: &str,
 #[pyo3(signature = (nesting=false, custom_media=false, deep_selector_combinator=false))]
 fn calc_parser_flags(nesting: bool, custom_media: bool, deep_selector_combinator: bool) -> u8 {
     let mut flags = 0;
-    if nesting { flags |= 1 << 0; }
-    if custom_media { flags |= 1 << 1; }
-    if deep_selector_combinator { flags |= 1 << 2; }
+    if nesting {
+        flags |= 1 << 0;
+    }
+    if custom_media {
+        flags |= 1 << 1;
+    }
+    if deep_selector_combinator {
+        flags |= 1 << 2;
+    }
     return flags;
 }
 
-fn mk_parser_options(filename: &str,
-                     error_recovery: bool,
-                     parser_flags: u8) -> ParserOptions {
+fn mk_parser_options(filename: &str, error_recovery: bool, parser_flags: u8) -> ParserOptions {
     return ParserOptions {
         filename: filename.to_string(),
         error_recovery: error_recovery,
@@ -94,18 +120,21 @@ fn mk_targets(browsers_list: Option<Vec<String>>) -> Result<Targets, Browserslis
     }
 }
 
-fn mk_minify_options(unused_symbols: Option<HashSet<String>>,
-                     targets: &Targets) -> MinifyOptions {
-    let mut options = MinifyOptions {targets: *targets, ..Default::default()};
+fn mk_minify_options(unused_symbols: Option<HashSet<String>>, targets: &Targets) -> MinifyOptions {
+    let mut options = MinifyOptions {
+        targets: *targets,
+        ..Default::default()
+    };
     match unused_symbols {
-        Some(val) => { options.unused_symbols = val; }
+        Some(val) => {
+            options.unused_symbols = val;
+        }
         None => {}
     }
     return options;
 }
 
-fn mk_printer_options<'a>(targets: &Targets,
-                          minify: bool) -> PrinterOptions<'a> {
+fn mk_printer_options<'a>(targets: &Targets, minify: bool) -> PrinterOptions<'a> {
     return PrinterOptions {
         minify: minify,
         targets: *targets,
